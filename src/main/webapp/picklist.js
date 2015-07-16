fa5988deec812e55e42c6ca960033811d2a6fa2a
onealanguin@gmail.com <Apple@581979>
2015-07-15 19:30:02 -0800

    Closes #6. Fixed lint errors shown when running grunt jshint against picklit.js.

--- src/main/webapp/picklist.js
+++ src/main/webapp/picklist.js
@@ -1,6 +1,6 @@
-
+'use strict';
 		
-function fxPickList($compile, $templateCache){
+function fxPickList(){
 	
 	/*
 	 * what do we need
@@ -10,7 +10,7 @@ function fxPickList($compile, $templateCache){
 	return {
 		replace:true,
 		priority:99,
-		templateUrl:"picklist.html",
+		templateUrl:'picklist.html',
 		restrict: 'E',
 		terminal:false,
 		scope:true, //do not pollute parent scope
@@ -36,34 +36,35 @@ function fxPickList($compile, $templateCache){
 			 * model directive is never processed.
 			 * The new ctrl is the subform.
 			 */
-			 delete($attrs['ngModel']);
+			 //delete($attrs['ngModel']);
+			 delete($attrs.ngModel);
 	         $element.removeAttr('data-ng-model');
 	         $element.removeAttr('ng-model');
 			
 			
 			//hold the selection.
-			$scope.picklist_src=[];
-			$scope.picklist_dest=[];
+			$scope.picklistSrc=[];
+			$scope.picklistDest=[];
 			
 			// match parsing is from angular js
 			var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/;
 			var match= $scope.ngOptions.match(NG_OPTIONS_REGEXP);
 			$scope.srcoptionsExp = match[7];
-			var displayFn = $parse(match[2] || match[1]),
-			valueName = match[4] || match[6],
-			keyName = match[5],
-			groupByFn = $parse(match[3] || ''),
-			valueFn = $parse(match[2] ? match[1] : valueName),
-			valuesFn = $parse(match[7]),
-			track = match[8],
-			trackFn = track ? $parse(match[8]) : null;
+			//var displayFn = $parse(match[2] || match[1]);
+			var valueName = match[4] || match[6];
+			//var keyName = match[5];
+			//var groupByFn = $parse(match[3] || '');
+			var valueFn = $parse(match[2] ? match[1] : valueName);
+			//var valuesFn = $parse(match[7]);
+			//var track = match[8];
+			//var trackFn = track ? $parse(match[8]) : null;
 			
 			var hasValueFn=match[2]?true:false;
 			
 			var modelFn=$parse($scope.ngModel);
 			var models=modelFn($scope);
 			//give models an empty array if needed.
-			if (!angular.isDefined(models) || models==null){
+			if (!angular.isDefined(models) || models===null){
 				modelFn.assign($scope, []);
 			}
 			
@@ -121,57 +122,57 @@ function fxPickList($compile, $templateCache){
 	        //do not update desttoptions to model.
 			$scope.rightShift=function(){
 				 var models=modelFn($scope);
-				for (var i=0; i<$scope.picklist_src.length; i++){
-					models.push($scope.itemToValue($scope.picklist_src[i]));
+				for (var i=0; i<$scope.picklistSrc.length; i++){
+					models.push($scope.itemToValue($scope.picklistSrc[i]));
 				}
-				$scope.removeLeftFromRight($scope.picklist_src, $scope.srcoptions);
-				$scope.picklist_src.length=0;
+				$scope.removeLeftFromRight($scope.picklistSrc, $scope.srcoptions);
+				$scope.picklistSrc.length=0;
 			};
 			$scope.rightShiftAll=function(){
 				 var models=modelFn($scope);
 				for (var i=0; i<$scope.srcoptions.length; i++){
 					models.push($scope.itemToValue($scope.srcoptions[i]));
 				}
-				$scope.picklist_src.length=0;
+				$scope.picklistSrc.length=0;
 				$scope.srcoptions.length=0;
 			};
 			$scope.leftShift=function(){
 				 var models=modelFn($scope);
-				for (var i=0; i<$scope.picklist_dest.length; i++){
-					$scope.srcoptions.push($scope.picklist_dest[i]);
+				for (var i=0; i<$scope.picklistDest.length; i++){
+					$scope.srcoptions.push($scope.picklistDest[i]);
 				}
-				$scope.removeLeftFromRight($scope.picklist_dest, $scope.destoptions);
+				$scope.removeLeftFromRight($scope.picklistDest, $scope.destoptions);
 				while(models.length>0){
 					models.pop();
 				}
 				angular.forEach($scope.destoptions, function(item){
 					models.push($scope.itemToValue(item));
 				});
-				$scope.picklist_dest.length=0;
+				$scope.picklistDest.length=0;
 			};
 			
 			$scope.leftShiftAll=function(){
 				for (var i=0; i<$scope.destoptions.length; i++){
 					$scope.srcoptions.push($scope.destoptions[i]);
 				}
-				$scope.picklist_dest.length=0;
+				$scope.picklistDest.length=0;
 				$scope.destoptions.length=0;
 				while(models.length>0){
 					models.pop();
 				}
 			};
 			$scope.arrowUp=function(){
-				var idxs=new Array();
-				for (var i=0; i<$scope.picklist_dest.length; i++){
+				var idxs= [];
+				for (var i=0; i<$scope.picklistDest.length; i++){
 					for (var j=0; j<$scope.destoptions.length; j++){
-						if($scope.picklist_dest[i]==$scope.destoptions[j]){
+						if($scope.picklistDest[i]===$scope.destoptions[j]){
 							idxs.push(j);
 						}
 					}
 				}
 				idxs.sort();
-				for (var i=0; i<idxs.length; i++){
-					var idx=idxs[i];
+				for (var m=0; m<idxs.length; m++){
+					var idx=idxs[m];
 					if (idx>0){
 						var temp=$scope.destoptions[idx-1];
 						$scope.destoptions[idx-1]=$scope.destoptions[idx];
@@ -186,17 +187,17 @@ function fxPickList($compile, $templateCache){
 				});
 			};
 			$scope.arrowDown=function(){
-				var idxs=new Array();
-				for (var i=0; i<$scope.picklist_dest.length; i++){
+				var idxs= [];
+				for (var i=0; i<$scope.picklistDest.length; i++){
 					for (var j=0; j<$scope.destoptions.length; j++){
-						if($scope.picklist_dest[i]==$scope.destoptions[j]){
+						if($scope.picklistDest[i]===$scope.destoptions[j]){
 							idxs.push(j);
 						}
 					}
 				}
 				idxs.sort();
-				for (var i=idxs.length-1; i>=0; i--){
-					var idx=idxs[i];
+				for (var l=idxs.length-1; l>=0; l--){
+					var idx=idxs[l];
 					if (idx<$scope.destoptions.length-1){
 						var temp=$scope.destoptions[idx+1];
 						$scope.destoptions[idx+1]=$scope.destoptions[idx];
@@ -233,7 +234,7 @@ function fxPickList($compile, $templateCache){
 
 
 		
-function fxPickListForm($compile, $templateCache){
+function fxPickListForm(){
 	
 	/*
 	 * what do we need
@@ -241,7 +242,7 @@ function fxPickListForm($compile, $templateCache){
 	 * 2. dest inst to put the select values: ng-model
 	 */
 	return {
-		require: "^picklist",
+		require: '^picklist',
 		priority:99,
 		restrict: 'A',
 		controller: function($scope, $element, $attrs){
@@ -254,7 +255,7 @@ function fxPickListForm($compile, $templateCache){
 }
 
 		
-function fxPickListSrc($compile, $templateCache){
+function fxPickListSrc(){
 	
 	/*
 	 * what do we need
@@ -262,21 +263,21 @@ function fxPickListSrc($compile, $templateCache){
 	 * 2. dest inst to put the select values: ng-model
 	 */
 	return {
-		require: "^picklist",
+		require: '^picklist',
 		priority:99,
 		restrict: 'A',
 		controller: function($scope, $element, $attrs){
 			$attrs.ngOptions=$scope.ngOptions;
 			$attrs.size=$scope.size;
-			$element.attr("size", $scope.size);
-			$attrs.name=$scope.formname+"_src";
+			$element.attr('size', $scope.size);
+			$attrs.name=$scope.formname+'_src';
 			//always use item as value.
-			$attrs.ngOptions=$scope.ngOptions.replace($scope.srcoptionsExp, "srcoptions").replace(/^.+\s+as\s+/, "");
+			$attrs.ngOptions=$scope.ngOptions.replace($scope.srcoptionsExp, 'srcoptions').replace(/^.+\s+as\s+/, '');
 		}
 	};
 }
 
-function fxPickListDest($compile, $templateCache){
+function fxPickListDest(){
 	
 	/*
 	 * what do we need
@@ -284,76 +285,76 @@ function fxPickListDest($compile, $templateCache){
 	 * 2. dest inst to put the select values: ng-model
 	 */
 	return {
-		require: "^picklist",
+		require: '^picklist',
 		priority:99,
 		restrict: 'A',
 		controller: function($scope, $element, $attrs){
 			$attrs.size=$scope.size;
-			$element.attr("size", $scope.size);
+			$element.attr('size', $scope.size);
 			//always use item as value. real valye is synchoinized to model by watcher function
-			$attrs.ngOptions=$scope.ngOptions.replace($scope.srcoptionsExp, "destoptions").replace(/^.+\s+as\s+/, "");
-			$attrs.name=$scope.formname+"_dest";
+			$attrs.ngOptions=$scope.ngOptions.replace($scope.srcoptionsExp, 'destoptions').replace(/^.+\s+as\s+/, '');
+			$attrs.name=$scope.formname+'_dest';
 		}
 	};
 }
 
 var fxPickListTpl=
-"			  <div style=\"display: table;width:100%\" data-ng-form=\"fake\" data-picklist-form>"+
-"		<div style=\"display: table-row;\">"+
-"			<div style=\"display: table-cell; width: 40%;vertical-align: middle;\">"+
-"				<select multiple size=\"5\" class=\"form-control\" data-ng-options=\"fake\" name=\"fake\" data-ng-model=\"picklist_src\" data-picklist-src>"+
-"				</select>"+
-"			</div>"+
-"			<div style=\"display: table-cell; width: 10%; vertical-align: middle;\" class=\"btn-group-vertical\">"+
-"				<div>"+
-"					<button type=\"button\" class=\"btn btn-info\" data-ng-click=\"rightShift();\">"+
-"						<span class=\"glyphicon glyphicon-step-forward\"></span>"+
-"					</button>"+
-"				</div>"+
-"				<div>"+
-"					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"rightShiftAll();\">"+
-"						<span class=\"glyphicon glyphicon-fast-forward\"></span>"+
-"					</button>"+
-"				</div>"+
-"				<div>"+
-"					<button type=\"button\" class=\"btn btn-info\" data-ng-click=\"leftShift();\">"+
-"						<span class=\"glyphicon glyphicon-step-backward\"></span>"+
-"					</button>"+
-"				</div>"+
-"				<div>"+
-"					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"leftShiftAll();\">"+
-"						<span class=\"glyphicon glyphicon-fast-backward\"></span>"+
-"					</button>"+
-"				</div>"+
-"			</div>"+
-"			<div style=\"display: table-cell; width: 40%;vertical-align: middle;\">"+
-"				<select multiple size=\"5\" class=\"form-control\" data-ng-options=\"fake\" name=\"fake\" data-ng-model=\"picklist_dest\" data-picklist-dest>"+
-"				</select>"+
-"			</div>"+
-"			<div style=\"display: table-cell; width: 10%; vertical-align: middle;\" class=\"btn-group-vertical\">"+
-"				<div>"+
-"					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"arrowUp();\">"+
-"						<span class=\"glyphicon glyphicon-arrow-up\"></span>"+
-"					</button>"+
-"				</div>"+
-"				<div>"+
-"					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"arrowDown();\">"+
-"						<span class=\"glyphicon glyphicon-arrow-down\"></span>"+
-"					</button>"+
-"				</div>"+
-"			</div>"+
-"		</div>"+
-"	</div>	  "
+'			  <div style=\"display: table;width:100%\" data-ng-form=\"fake\" data-picklist-form>'+
+'		<div style=\"display: table-row;\">'+
+'			<div style=\"display: table-cell; width: 40%;vertical-align: middle;\">'+
+'				<select multiple size=\"5\" class=\"form-control\" data-ng-options=\"fake\" name=\"fake\" data-ng-model=\"picklistSrc\" data-picklist-src>'+
+'				</select>'+
+'			</div>'+
+'			<div style=\"display: table-cell; width: 10%; vertical-align: middle;\" class=\"btn-group-vertical\">'+
+'				<div>'+
+'					<button type=\"button\" class=\"btn btn-info\" data-ng-click=\"rightShift();\">'+
+'						<span class=\"glyphicon glyphicon-step-forward\"></span>'+
+'					</button>'+
+'				</div>'+
+'				<div>'+
+'					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"rightShiftAll();\">'+
+'						<span class=\"glyphicon glyphicon-fast-forward\"></span>'+
+'					</button>'+
+'				</div>'+
+'				<div>'+
+'					<button type=\"button\" class=\"btn btn-info\" data-ng-click=\"leftShift();\">'+
+'						<span class=\"glyphicon glyphicon-step-backward\"></span>'+
+'					</button>'+
+'				</div>'+
+'				<div>'+
+'					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"leftShiftAll();\">'+
+'						<span class=\"glyphicon glyphicon-fast-backward\"></span>'+
+'					</button>'+
+'				</div>'+
+'			</div>'+
+'			<div style=\"display: table-cell; width: 40%;vertical-align: middle;\">'+
+'				<select multiple size=\"5\" class=\"form-control\" data-ng-options=\"fake\" name=\"fake\" data-ng-model=\"picklistDest\" data-picklist-dest>'+
+'				</select>'+
+'			</div>'+
+'			<div style=\"display: table-cell; width: 10%; vertical-align: middle;\" class=\"btn-group-vertical\">'+
+'				<div>'+
+'					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"arrowUp();\">'+
+'						<span class=\"glyphicon glyphicon-arrow-up\"></span>'+
+'					</button>'+
+'				</div>'+
+'				<div>'+
+'					<button type=\"button\" class=\"btn btn-default\" data-ng-click=\"arrowDown();\">'+
+'						<span class=\"glyphicon glyphicon-arrow-down\"></span>'+
+'					</button>'+
+'				</div>'+
+'			</div>'+
+'		</div>'+
+'	</div>	  '
 ;
 
 		
 
-angular.module("fxpicklist", [])
-.directive("picklist", fxPickList)
-.directive("picklistForm", fxPickListForm)
-.directive("picklistSrc", fxPickListSrc) 
-.directive("picklistDest", fxPickListDest)
-.run(["$templateCache", function($templateCache) {
-	 $templateCache.put("picklist.html", fxPickListTpl);
+angular.module('fxpicklist', [])
+.directive('picklist', fxPickList)
+.directive('picklistForm', fxPickListForm)
+.directive('picklistSrc', fxPickListSrc) 
+.directive('picklistDest', fxPickListDest)
+.run(['$templateCache', function($templateCache) {
+	 $templateCache.put('picklist.html', fxPickListTpl);
 }]);
 	 
